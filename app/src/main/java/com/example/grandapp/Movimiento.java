12903f893a55a1f9b0a106731c852b1344b4dbed
melaniemauas <melaniemauas@gmail.com>
2021-06-11 11:58:30 -0300

    youtube

--- app/src/main/java/com/example/grandapp/Movimiento.java
+++ app/src/main/java/com/example/grandapp/Movimiento.java
@@ -7,12 +7,18 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
 import java.util.ArrayList;
 
-public class Movimiento extends AppCompatActivity {
+public class Movimiento extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     Spinner ListaTemas;
     ArrayList<String> TemasLista;
     ArrayAdapter<String> Adaptador;
+    YouTubePlayerView YouTubePlayerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,17 @@ public class Movimiento extends AppCompatActivity {
         Log.d("array", "" + Adaptador);
         ListaTemas=(Spinner)findViewById(R.id.lista);
         ListaTemas.setAdapter(Adaptador);
+        YouTubePlayerView=(YouTubePlayerView)findViewById(R.id.youtube_view);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
     }
 }
