e033f2ee293b0de3ec8b2fa984437d9374b8c9e7
Tomas Jalil <35005396+TomasJalil@users.noreply.github.com>
2021-06-18 12:01:36 -0300

    nos eeeee nuevo

--- app/src/main/java/com/example/grandapp/MovimientoElegido.java
+++ app/src/main/java/com/example/grandapp/MovimientoElegido.java
@@ -37,6 +37,9 @@ public class MovimientoElegido extends AppCompatActivity {
         Log.d("array", "" + Adaptador);
         ListaVideos = findViewById(R.id.lista);
         ListaVideos.setAdapter(Adaptador);
+        TareaAsincronicaVideo LecturaTemas;
+        LecturaTemas = new TareaAsincronicaVideo();
+        LecturaTemas.execute();
 
     }
     private class TareaAsincronicaVideo extends AsyncTask<Void, Void, Void> {
@@ -70,30 +73,81 @@ public class MovimientoElegido extends AppCompatActivity {
 
             Log.d("postExecute", "onpost eexecute bien");
         }
-    }
 
-    public void cargarLista(JsonArray VideoLista) {
-        String video;
-        for (int x = 0; x < VideoLista.size(); x++) {
-            JsonObject unaCategoria = VideoLista.get(x).getAsJsonObject();
-            video = unaCategoria.get("video").getAsString();
-          VideosLista.add(new Tema(idTema,tema));
+        public void ProcesarJsonLeidoNombres(InputStreamReader respLeidaNombre) {
+            Log.d("andaprocesar", "ProcesarJsonLeidoNombres: ");
+            JsonParser ProcesadorDeJson;
+            ProcesadorDeJson = new JsonParser();
+            JsonObject objetoJson;
+            JsonArray VideoLista;
+            objetoJson = ProcesadorDeJson.parse(respLeidaNombre).getAsJsonObject();
+            VideoLista = objetoJson.get("video").getAsJsonArray();
+            TareaAsincronicaYoutube Lectura;
+            Lectura = new TareaAsincronicaYoutube();
+            Lectura.execute();
 
         }
 
+        private class TareaAsincronicaYoutube extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                try {
+                    String url = ("localhost:5000/videos/:idTema");
+                    Log.d("url", "doInBackground:Url y NombreCta " + url);
+                    URL miRuta = new URL(url);
+                    HttpURLConnection miConexion = (HttpURLConnection) miRuta.openConnection();
+                    if (miConexion.getResponseCode() == 200) {
+
+                        InputStream cuerpoRespuesta = miConexion.getInputStream();
+                        InputStreamReader respLeidaNombre = new InputStreamReader(cuerpoRespuesta, "UTF-8");
+                        ProcesarJsonLeidoYoutube(respLeidaNombre);
+                    }
+                } catch (Exception error) {
+                    Log.d("PresionoBoton", "no se creo una URL" + error.getMessage());
+                }
+                return null;
+            }
+
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+
+                ListaVideos.setAdapter(Adaptador);
+                Adaptador.notifyDataSetChanged();
+
+                Log.d("postExecute", "onpost eexecute bien");
+            }
+        }
+
+        public void cargarLista(JsonArray VideoLista) {
+            String foto,descripcion,titulo;
+            for (int x = 0; x < VideoLista.size(); x++) {
+                JsonObject unaCategoria = VideoLista.get(x).getAsJsonObject();
+                foto = unaCategoria.get("foto").getAsString();
+                descripcion = unaCategoria.get("descripcion").getAsString();
+                titulo = unaCategoria.get("titulo").getAsString();
+
+                VideosLista.add(new Video(idTema, tema));
+
+            }
+
+        }
+
+        public void ProcesarJsonLeidoYoutube(InputStreamReader respLeidaNombre) {
+            Log.d("andaprocesar", "ProcesarJsonLeidoNombres: ");
+            JsonParser ProcesadorDeJson;
+            ProcesadorDeJson = new JsonParser();
+            JsonObject objetoJson;
+            JsonArray VideoLista;
+            objetoJson = ProcesadorDeJson.parse(respLeidaNombre).getAsJsonObject();
+            VideoLista = objetoJson.get("video").getAsJsonArray();
+            cargarLista(VideoLista);
+        }
+
+
     }
 
-    public void ProcesarJsonLeidoNombres(InputStreamReader respLeidaNombre) {
-        Log.d("andaprocesar", "ProcesarJsonLeidoNombres: ");
-        JsonParser ProcesadorDeJson;
-        ProcesadorDeJson = new JsonParser();
-        JsonObject objetoJson;
-        JsonArray VideoLista;
-        objetoJson = ProcesadorDeJson.parse(respLeidaNombre).getAsJsonObject();
-        VideoLista= objetoJson.get("video").getAsJsonArray();
-        cargarLista(VideoLista);
-    }
-
-
-    }
+}
 
--- app/src/main/java/com/example/grandapp/Video.java
+++ app/src/main/java/com/example/grandapp/Video.java
@@ -23,7 +23,7 @@ public class Video {
        this.Titulo=titulo;
        return Titulo;
    }
-    public String Foto(String foto){
+    public String getFoto(String foto){
         this.Foto=foto;
         return Foto;
     }
@@ -34,6 +34,30 @@ public class Video {
     public Integer getCantVisitas(Integer cantVistas){
         this.CantVistas=cantVistas;
         return CantVistas;
+    }
+    public void setFoto(String foto) {
+        Foto = foto;
+    }
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+    public void setUrlVideo(String urlVideo) {
+        UrlVideo = urlVideo;
+    }
+    public void setCantVistas(Integer cantVisitas) {
+        CantVistas = cantVisitas;
+    }
+    public Video( String foto,String descripcion, String titulo, String urlVideo) {
+        setFoto( foto);
+        setDescripcion( descripcion);
+        setTitulo( titulo);
+        setUrlVideo(urlVideo);
+
+
+
     }
     }
 
